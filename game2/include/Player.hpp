01d721aa78828bbc68c5aa3d47180c75994c37a5
luongkien25 <kluong960@gmail.com>
2025-08-08 22:48:49 +0700

    Update Player.hpp

--- game2/include/Player.hpp
+++ game2/include/Player.hpp
@@ -10,11 +10,14 @@
 #include<SDL2/SDL_ttf.h>
 
 using namespace std;
-
+#include <iomanip>
+#include <sstream>
 class Player{
 
  public:
      Player(SDL_Renderer* renderer);
+     string formatTimeMMSS(Uint32 ms);
+     void handle_timer(Uint32& current_time);
      void load_player_rack(const string& path);
      void generate_letters();
      void rack_update();
@@ -29,13 +32,27 @@ class Player{
      vector<Tile> letters;
      vector<Tile> player2_letters;///?
      std::vector<std::pair<int, int>> tile_positions;
+     std::vector<std::pair<int, int>> tile_positions2;
      SDL_Rect rack_rect;
      SDL_Rect dst_rect;
+     bool is_first_player_turn = false;
+     int score = 0,player1_score = 0,player2_score =0 ;
+     string player1_score_text = "Score: ",player2_score_text = "Score: ";
+     void set_player1_turn_start(Uint32 tick) { player1_turn_start = tick; }
+     void set_player2_turn_start(Uint32 tick) { player2_turn_start = tick; }
+     Uint32 get_player1_time_left() const { return player1_time_left; }
+     Uint32 get_player2_time_left() const { return player2_time_left; }
+    void update_score_texts();
  private:
+    Uint32 player1_turn_start = 0;
+    Uint32 player2_turn_start = 0;
      SDL_Renderer* renderer;
      SDL_Texture* rack;
 
      int letters_size = 7;
      int tile_size = 40;
-     int score = 0;
+
+     Uint32 player1_time_left;
+     Uint32 player2_time_left;
+     Uint32 time_per_match = 20000;
 };
